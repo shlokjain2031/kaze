a54eea93727de58efc3ed2413e6023340036610f
shlokjain2031 <jainshlok20@protonmail.com>
2021-05-19 10:04:12 +0530

    made into a launcher
    TODO: notifications, prompts

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -38,126 +38,42 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: colours.black(),
-      body: FutureBuilder<List<ModeModel>>(
-        future: ModeService().getAllModes(),
-        builder: (context, snapshot) {
-          if(snapshot.hasData) {
-            List<ModeModel> allModes = snapshot.data;
+    return WillPopScope(
+      onWillPop: () => Future(() => false),
+      child: Scaffold(
+        backgroundColor: colours.black(),
+        body: FutureBuilder<List<ModeModel>>(
+          future: ModeService().getAllModes(),
+          builder: (context, snapshot) {
+            if(snapshot.hasData) {
+              List<ModeModel> allModes = snapshot.data;
 
-            return PageView.builder(
-              itemCount: (allModes.length + 1),
-              controller: _pageController,
-              scrollDirection: Axis.vertical,
-              onPageChanged: (val) {
-                setState(() {});
-              },
-              itemBuilder: (context, index) {
-                if (index == allModes.length) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: colours.black(),
-                      border: Border(
-                        bottom: BorderSide(color: colours.white(), width: 5)
-                      )
-                    ),
-                    child: Column(
-                      children: [
-                        _pageController.page.toInt() == (index) ? Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                'mode',
-                                style: TextStyle(
-                                    fontFamily: 'ProductSans',
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: colours.white(opacity: .9)
-                                ),
-                              ),
-                              Image(
-                                image: AssetImage('assets/category.png'),
-                                width: 32,
-                                color: colours.white(),
-                                fit: BoxFit.fill,
-                              )
-                            ],
-                          ),
-                        ) : SizedBox(),
-
-                        SizedBox(height: sizes.height(context, 200)),
-                        Text(
-                          "Add +",
-                          style: TextStyle(
-                              fontSize: 54,
-                              fontFamily: 'ProductSans',
-                              fontWeight: FontWeight.bold,
-                              color: colours.white(opacity: .9),
-                              shadows: [
-                                Shadow(
-                                    offset: Offset(8, 8),
-                                    blurRadius: 32,
-                                    color: colours.black(opacity: .6)
-                                )
-                              ]
-                          ),
-                        ),
-                        SizedBox(height: 18),
-                        Text(
-                          'click to add a mode',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontFamily: 'ProductSans',
-                            color: colours.white(opacity: .8),
-                          ),
-                        ),
-                        SizedBox(height: sizes.height(context, 48)),
-                      ],
-                    ),
-                  );
-                }
-                else {
-                  ModeModel mode = allModes[index];
-                  DateTime startTime = DateTime.parse(mode.startTime);
-                  DateTime endTime = DateTime.parse(mode.endTime);
-
-                  return Container(
-                    decoration: mode.wallpaperPath != null ? BoxDecoration(
-                        image: DecorationImage(
-                          image: FileImage(File(mode.wallpaperPath)),
-                          fit: BoxFit.fitHeight,
-                          colorFilter: ColorFilter.mode(colours.black().withOpacity(0.7),
-                              BlendMode.dstATop),
-                        ),
+              return PageView.builder(
+                itemCount: (allModes.length + 1),
+                controller: _pageController,
+                scrollDirection: Axis.vertical,
+                onPageChanged: (val) {
+                  setState(() {});
+                },
+                itemBuilder: (context, index) {
+                  if (index == allModes.length) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: colours.black(),
                         border: Border(
-                            bottom: BorderSide(color: colours.white(), width: 5)
+                          bottom: BorderSide(color: colours.white(), width: 5)
                         )
-                    ) : BoxDecoration(color: colours.black()),
-                    child: Column(
-                      children: [
-                        _pageController.page.toInt() != (index) ? SizedBox() : Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (context) {
-                                        return Edit(mode: mode,);
-                                      },
-                                    ),
-                                  );
-                                },
-                                child: Text(
-                                  'edit mode',
+                      ),
+                      child: Column(
+                        children: [
+                          _pageController.page.toInt() == (index) ? Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'mode',
                                   style: TextStyle(
                                       fontFamily: 'ProductSans',
                                       fontSize: 20,
@@ -165,82 +81,169 @@ class _HomeState extends State<Home> {
                                       color: colours.white(opacity: .9)
                                   ),
                                 ),
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  CustomDialogs().category(context, sizes, colours);
-                                },
-                                child: Image(
+                                Image(
                                   image: AssetImage('assets/category.png'),
                                   width: 32,
                                   color: colours.white(),
                                   fit: BoxFit.fill,
+                                )
+                              ],
+                            ),
+                          ) : SizedBox(),
+
+                          SizedBox(height: sizes.height(context, 200)),
+                          Text(
+                            "Add +",
+                            style: TextStyle(
+                                fontSize: 54,
+                                fontFamily: 'ProductSans',
+                                fontWeight: FontWeight.bold,
+                                color: colours.white(opacity: .9),
+                                shadows: [
+                                  Shadow(
+                                      offset: Offset(8, 8),
+                                      blurRadius: 32,
+                                      color: colours.black(opacity: .6)
+                                  )
+                                ]
+                            ),
+                          ),
+                          SizedBox(height: 18),
+                          Text(
+                            'click to add a mode',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontFamily: 'ProductSans',
+                              color: colours.white(opacity: .8),
+                            ),
+                          ),
+                          SizedBox(height: sizes.height(context, 48)),
+                        ],
+                      ),
+                    );
+                  }
+                  else {
+                    ModeModel mode = allModes[index];
+                    DateTime startTime = DateTime.parse(mode.startTime);
+                    DateTime endTime = DateTime.parse(mode.endTime);
+
+                    return Container(
+                      decoration: mode.wallpaperPath != null ? BoxDecoration(
+                          image: DecorationImage(
+                            image: FileImage(File(mode.wallpaperPath)),
+                            fit: BoxFit.fitHeight,
+                            colorFilter: ColorFilter.mode(colours.black().withOpacity(0.7),
+                                BlendMode.dstATop),
+                          ),
+                          border: Border(
+                              bottom: BorderSide(color: colours.white(), width: 5)
+                          )
+                      ) : BoxDecoration(color: colours.black()),
+                      child: Column(
+                        children: [
+                          _pageController.page.toInt() != (index) ? SizedBox() : Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) {
+                                          return Edit(mode: mode,);
+                                        },
+                                      ),
+                                    );
+                                  },
+                                  child: Text(
+                                    'edit mode',
+                                    style: TextStyle(
+                                        fontFamily: 'ProductSans',
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: colours.white(opacity: .9)
+                                    ),
+                                  ),
                                 ),
-                              )
-                            ],
+                                GestureDetector(
+                                  onTap: () {
+                                    CustomDialogs().category(context, sizes, colours);
+                                  },
+                                  child: Image(
+                                    image: AssetImage('assets/category.png'),
+                                    width: 32,
+                                    color: colours.white(),
+                                    fit: BoxFit.fill,
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
-                        ),
 
-                        SizedBox(height: sizes.height(context, 200)),
-                        Text(
-                          mode.title,
-                          style: TextStyle(
-                            fontSize: 54,
-                            fontFamily: 'ProductSans',
-                            fontWeight: FontWeight.bold,
-                            color: colours.white(opacity: .9),
-                            shadows: [
-                              Shadow(
-                                offset: Offset(8, 8),
-                                blurRadius: 32,
-                                color: colours.black(opacity: .6)
-                              )
-                            ]
+                          SizedBox(height: sizes.height(context, 200)),
+                          Text(
+                            mode.title,
+                            style: TextStyle(
+                              fontSize: 54,
+                              fontFamily: 'ProductSans',
+                              fontWeight: FontWeight.bold,
+                              color: colours.white(opacity: .9),
+                              shadows: [
+                                Shadow(
+                                  offset: Offset(8, 8),
+                                  blurRadius: 32,
+                                  color: colours.black(opacity: .6)
+                                )
+                              ]
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 18),
-                        Text(
-                          startTime.hour.toString() + ":" + (startTime.minute == 0 ? startTime.minute.toString() + "0" : startTime.minute.toString())
-                              + " - " + endTime.hour.toString() + ":" + (endTime.minute == 0 ? endTime.minute.toString() + "0" : endTime.minute.toString()),
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontFamily: 'ProductSans',
-                            color: colours.white(opacity: .8),
+                          SizedBox(height: 18),
+                          Text(
+                            startTime.hour.toString() + ":" + (startTime.minute == 0 ? startTime.minute.toString() + "0" : startTime.minute.toString())
+                                + " - " + endTime.hour.toString() + ":" + (endTime.minute == 0 ? endTime.minute.toString() + "0" : endTime.minute.toString()),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontFamily: 'ProductSans',
+                              color: colours.white(opacity: .8),
+                            ),
                           ),
-                        ),
-                        SizedBox(height: sizes.height(context, 48)),
+                          SizedBox(height: sizes.height(context, 48)),
 
-                        // ListView.builder(
-                        //   itemCount: 4,
-                        //   scrollDirection: Axis.horizontal,
-                        //   itemBuilder: (context, index) {
-                        //     return Container(
-                        //       width: sizes.width(context, 50),
-                        //       height: sizes.height(context, 64),
-                        //       margin: EdgeInsets.only(right: 12),
-                        //       decoration: BoxDecoration(
-                        //           shape: BoxShape.circle,
-                        //           color: colours.white() // change with colours.black()
-                        //       ),
-                        //       child: Image(
-                        //         image: MemoryImage(apps[index]["icon"]),
-                        //         fit: BoxFit.fill,
-                        //       ),
-                        //     );
-                        //   },
-                        // )
-                      ],
-                    ),
-                  );
-                }
-              },
-            );
+                          // ListView.builder(
+                          //   itemCount: 4,
+                          //   scrollDirection: Axis.horizontal,
+                          //   itemBuilder: (context, index) {
+                          //     return Container(
+                          //       width: sizes.width(context, 50),
+                          //       height: sizes.height(context, 64),
+                          //       margin: EdgeInsets.only(right: 12),
+                          //       decoration: BoxDecoration(
+                          //           shape: BoxShape.circle,
+                          //           color: colours.white() // change with colours.black()
+                          //       ),
+                          //       child: Image(
+                          //         image: MemoryImage(apps[index]["icon"]),
+                          //         fit: BoxFit.fill,
+                          //       ),
+                          //     );
+                          //   },
+                          // )
+                        ],
+                      ),
+                    );
+                  }
+                },
+              );
+            }
+            else {
+              return SizedBox();
+            }
           }
-          else {
-            return SizedBox();
-          }
-        }
+        ),
       ),
     );
   }
